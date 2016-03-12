e01350556f3194a5a7e79b3078baebeb733d4dc0
Christian1994 <crisdanovagoodyear@hotmail.com>
2016-03-12 11:44:16 -0500

    Creación del bosquejo de la Capa de Lógica.

---
+++ src/logica/LogicaMateria.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logica;
+
+/**
+ *
+ * @author NOREÑA
+ */
+public class LogicaMateria {
+    
+    // Validación para crear materia
+    
+    // Validación para editar materia
+    
+    // Validación para eliminar materia
+    
+    // Validación para consultar materia
+    
+}
