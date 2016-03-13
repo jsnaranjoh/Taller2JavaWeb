b075725496b13b12f2a344851a6a4c377c843fad
Christian1994 <crisdanovagoodyear@hotmail.com>
2016-03-13 15:53:10 -0500

    Modificación a LógicaMateria y VistaMateria.

--- src/logica/LogicaMateria.java
+++ src/logica/LogicaMateria.java
@@ -5,13 +5,45 @@
  */
 package logica;
 
+import modelo.Materia;
+import persistencia.MateriaJpaController;
+
 /**
  *
  * @author NOREÑA
  */
 public class LogicaMateria {
     
+    MateriaJpaController materiaDAO;
+    
     // Validación para crear materia
+    public void crearMateria(Materia materia) throws Exception{
+        
+        // Evaluación de campos vacíos
+        if(materia == null){
+            throw new Exception("Campos vacíos.");
+        }
+        else{
+            if(materia.getNumeromateria() == 0 || materia.getNumeromateria() == null){
+                throw new Exception("Campo No. Materia obligatorio.");
+            }
+            if(materia.getNombremateria() == null ||materia.getNombremateria().equals("")){
+                throw new Exception("Campo Nombre obligatorio.");
+            }
+            if(materia.getCreditosmateria() == 0 || materia.getCreditosmateria() == null){
+                throw new Exception("Campo No. Créditos obligatorio.");
+            }
+        }
+        
+        // Evaluación de existencia de una materia
+        Materia objMateria = materiaDAO.findMateria(materia.getNumeromateria());
+        if(objMateria != null){
+            throw new Exception("Materia ya existe.");
+        }
+        else{
+            materiaDAO.create(materia);
+        }
+    }
     
     // Validación para editar materia
     
